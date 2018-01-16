c5e4caedb376e4405de38ee3088b20525321d329
MrTrainer201 <ephraimjackson3@gmail.com>
2018-01-16 16:53:42 -0600

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	//A bool that determines if the game is paused
 	private bool paused;
+	//A GameObject reference to the player
 	GameObject Player;
 
+	//References to various UI elements
 	[SerializeField]
-	private GameObject PauseUI;
+	private GameObject PauseUI; //Pause text
 	[SerializeField]
-	private GameObject RetryButton;
+	private GameObject RetryButton; //Retry button
 	[SerializeField]
-	private GameObject GameoveUI;
+	private GameObject GameoveUI; //Gameover UI which containes Gameover Text, along with Retry, Mainmenu, and Quit buttons
 	[SerializeField]
-	private GameObject Gameovertext;
+	private GameObject Gameovertext; //Game over Text
 
-
+	//Allows public access to paused bool
 	public bool Paused
 	{
 		get
@@ -43,12 +46,14 @@ public class GameManager : MonoBehaviour {
 
 	void Start ()
 	{
+		//Establish reference to the player
 		Player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
+		//Pause the game if the player pushed start or q and is not dead.
 		if (Input.GetKeyDown ("q") || Input.GetButtonDown ("Start Button"))
 		{
 			if (Player != null) 
@@ -58,12 +63,16 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
+	//Function that pauses or unpauses the game
 	public void PauseGame ()
 	{
+		//Swaps paused bool between true and false
 		paused = !paused;
 
+		//Swap visibility of Pause and Game over UI
 		PauseUI.SetActive (paused);
 		GameoveUI.SetActive (paused);
+		//Always hide game over text and retry button.
 		Gameovertext.SetActive (false);
 		RetryButton.SetActive (false);
 	}
